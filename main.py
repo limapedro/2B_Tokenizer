c2d49fd58095f789d06bca72cf67cac2f1414835
limapedro <Pedrolimasilva17@gmail.com>
2024-09-02 14:41:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class BytePairTokenizer:
 tokenizer = BytePairTokenizer()
 
 # Example input (binary data)
-input_data = "123"
+input_data = "hello world! how is this even a thing 122334esdsfd~´"
 encoded_tokens = tokenizer.encode(input_data)
 print(f"Encoded Tokens: {encoded_tokens}")
 
