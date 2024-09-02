1b206a24f50d34f60312d67693b1e23c18fa42b4
limapedro <Pedrolimasilva17@gmail.com>
2024-09-02 15:07:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class BytePairTokenizer:
 tokenizer = BytePairTokenizer()
 
 # Example input (binary data)
-input_data = "I will meet you amanhã para um café!"
+input_data = "I will meet."
 encoded_tokens = tokenizer.encode(input_data)
 print(f"Encoded Tokens: {encoded_tokens}")
 
